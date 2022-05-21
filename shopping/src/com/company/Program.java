a0bc173cde4b7c8e6f15f60610642a278230a07f
Elma Skopljak <105375038+Elmaaaaaaa@users.noreply.github.com>
2022-05-21 13:22:13 +0200

    Update Program.java

--- shopping/src/com/company/Program.java
+++ shopping/src/com/company/Program.java
@@ -8,6 +8,11 @@ public class Program {
                 300, true);
         myShopping1.setTypesOfClothes("Trousers");
         System.out.println(myShopping1.getTypesOfClothes());
-        System.out.println(myShopping1.getAward());
+        System.out.println("Lucky number: " + myShopping1.getAward());
+        System.out.println("Today's number of sales is: " + myShopping1.getNumberOfSales());
+	    Shopping myShopping2 = new Shopping("Skirt", 25, false, 301, false );
+        Shopping myShopping3 = new Shopping("Socks", 13.50, true, 302, true);
+        System.out.println("Lucky number: " + myShopping3.getAward());
+        System.out.println("Today's number of sales is: " + myShopping1.getNumberOfSales());
     }
 }
